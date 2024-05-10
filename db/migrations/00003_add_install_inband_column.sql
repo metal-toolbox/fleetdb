3751d627900affc4aceb4617090ddeae246852ee
Joel Rebello <jrebello@packet.com>
2024-05-10 15:50:30 +0200

    db/migrations: Add install_inband bool column to component_firmware_version

---
+++ db/migrations/00003_add_install_inband_column.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE public.component_firmware_version ADD COLUMN install_inband BOOL DEFAULT false;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE public.component_firmware_version DROP COLUMN install_inband;
+-- +goose StatementEnd

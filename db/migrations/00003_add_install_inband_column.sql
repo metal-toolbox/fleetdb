6da2c7d49b7675928515be55ae30b4cba4ca418b
Joel Rebello <jrebello@packet.com>
2024-05-14 16:04:27 +0200

    db/migrations: install_inband column must never be null

--- db/migrations/00003_add_install_inband_column.sql
+++ db/migrations/00003_add_install_inband_column.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE public.component_firmware_version ADD COLUMN install_inband BOOL DEFAULT false;
+ALTER TABLE public.component_firmware_version ADD COLUMN install_inband BOOL NOT NULL DEFAULT false;
 -- +goose StatementEnd
 
 -- +goose Down

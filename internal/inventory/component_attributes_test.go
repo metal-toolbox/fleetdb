ac8017af65fc3596ec17d77454f0ebbda59b81ac
Doctor Vince <DoctorVin@users.noreply.github.com>
2024-05-16 12:17:38 -0400

    encode status as a JSON array for backward compatibility

---
+++ internal/inventory/component_attributes_test.go
@@ -0,0 +1,65 @@
+package inventory
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/bmc-toolbox/common"
+	"github.com/stretchr/testify/require"
+)
+
+type clutter struct {
+	Payload string `json:"payload,omitempty"`
+}
+
+type testContainer struct {
+	Clutter *clutter       `json:"clutter,omitempty"`
+	Status  *common.Status `json:"status,omitempty"`
+}
+
+func TestStatusFromJSON(t *testing.T) {
+	t.Run("cluttered array", func(t *testing.T) {
+		ary := []*testContainer{
+			&testContainer{
+				Clutter: &clutter{
+					Payload: "stuff",
+				},
+			},
+			&testContainer{
+				Status: &common.Status{
+					Health: "great",
+					State:  "awesome",
+				},
+			},
+			&testContainer{
+				Clutter: &clutter{},
+			},
+		}
+		data, err := json.Marshal(ary)
+		require.NoError(t, err, "pre-requisite")
+		st, err := statusFromJSON(data)
+		require.NoError(t, err, "function call")
+		require.NotNil(t, st)
+		require.Equal(t, "great", st.Health)
+	})
+	t.Run("serialized empty object returns empty object", func(t *testing.T) {
+		ary := []*testContainer{
+			&testContainer{
+				Status: &common.Status{},
+			},
+		}
+		data, err := json.Marshal(ary)
+		require.NoError(t, err, "pre-requisite")
+		st, err := statusFromJSON(data)
+		require.NoError(t, err, "function call")
+		require.NotNil(t, st)
+	})
+	t.Run("empty non-nil array payload returns nil", func(t *testing.T) {
+		ary := []*testContainer{}
+		data, err := json.Marshal(ary)
+		require.NoError(t, err, "pre-requisite")
+		st, err := statusFromJSON(data)
+		require.NoError(t, err, "function call")
+		require.Nil(t, st)
+	})
+}
